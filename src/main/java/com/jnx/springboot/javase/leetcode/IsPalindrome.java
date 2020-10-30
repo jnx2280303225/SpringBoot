be56c8d6aebe184651c84e45fe72f132dd83ab17
jiangnanxin <ss2280303225@163.com>
2020-10-30 14:32:16 +0800

    feat:新增各类算法题目解答（贪心算法）

--- src/main/java/com/jnx/springboot/javase/leetcode/IsPalindrome.java
+++ src/main/java/com/jnx/springboot/javase/leetcode/IsPalindrome.java
@@ -20,6 +20,7 @@ public class IsPalindrome {
     public static void main(String[] args) {
         System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
         System.out.println(isPalindrome("race a car"));
+        System.out.println(isPalindrome("otto"));
     }
 
     /**
